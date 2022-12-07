912a221cbed9108777944bfbaaf3c43597b8c904
scode234 <scodes3451@gmail.com>
2022-12-06 19:06:31 -0600

    Add private variables

--- budgetTracker/src/budgetTracker/Household.java
+++ budgetTracker/src/budgetTracker/Household.java
@@ -1,5 +1,35 @@
 package budgetTracker;
 
-public class Household {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Household {
+private List<FamilyMember> householdMembers = new ArrayList<FamilyMember>();
+	
+	private double income;
+	private double expenses;
+	
+	private double housingBudget;
+	private double housingSpend;
+	
+	private double groceryBudget;
+	private double grocerySpend;
+	
+	private double diningBudget;
+	private double diningSpend;
+	
+	private double carBudget;
+	private double carSpend;
+	
+	private double utilitiesBudget;
+	private double utilitiesSpend;
+	
+	private double healthBudget;
+	private double healthSpend;
+	
+	private double funBudget;
+	private double funSpend;
+	
+	private double miscBudget;
+	private double miscSpend;
 }

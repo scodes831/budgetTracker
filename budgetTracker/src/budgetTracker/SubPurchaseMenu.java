52b0f5c3ef230023eb3a78efc92dc1fd67901b9f
scode234 <scodes3451@gmail.com>
2022-12-15 20:37:16 -0600

    Refactor SubPurchaseMenu to handle invalid input error

--- budgetTracker/src/budgetTracker/SubPurchaseMenu.java
+++ budgetTracker/src/budgetTracker/SubPurchaseMenu.java
@@ -5,10 +5,29 @@ import java.util.Scanner;
 public class SubPurchaseMenu implements SubMenu {
 
 	public void show(Household household, Budget budget, Menu purchaseMenu, Menu mainMenu) {
+		boolean selectionError;
+		do {
+			try {
+				String selection = showOptions();
+				selectionError = false;
+				processSelection(household, budget, purchaseMenu, mainMenu, selection);
+			} catch (Exception e) {
+				selectionError = true;
+				System.out.println("Please enter a valid selection.");
+			}
+		} while (selectionError);
+		
+	}
+
+	public String showOptions() {
 		System.out.println(
 				"Choose an option:\na - Show All Purchases\nb - Show Purchases By Family Member\nc - Show Purchases By Date\nd - Back to Purchase Menu");
 		Scanner in = new Scanner(System.in);
 		String selection = in.next();
+		return selection;
+	}
+
+	public void processSelection(Household household, Budget budget, Menu parentMenu, Menu mainMenu, String selection) {
 		switch (selection) {
 		case "a":
 			Purchase.showAllPurchases(household);
@@ -20,8 +39,8 @@ public class SubPurchaseMenu implements SubMenu {
 			Purchase.showPurchasesByDate(household.getPurchasesList());
 			break;
 		case "d":
-			purchaseMenu.show(household, budget, mainMenu);
+			parentMenu.show(household, budget, mainMenu);
 		}
-		show(household, budget, purchaseMenu, mainMenu);
+		show(household, budget, parentMenu, mainMenu);
 	}
 }

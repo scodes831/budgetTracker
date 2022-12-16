6cc5829c5d2c16194d31ffc28f378ac7a0c158ae
scode234 <scodes3451@gmail.com>
2022-12-15 20:30:48 -0600

    Refactor PurchaseMenu to handle invalid input error

--- budgetTracker/src/budgetTracker/PurchaseMenu.java
+++ budgetTracker/src/budgetTracker/PurchaseMenu.java
@@ -6,26 +6,17 @@ import java.util.Scanner;
 public class PurchaseMenu extends Menu {
 
 	public void show(Household household, Budget budget, Menu mainMenu) {
-		System.out.println("Purchase Menu Options:\n1 - Add a Purchase\n2 - Display Purchases\n3 - Edit a Purchase\n4 - Back to Main Menu");
-		Scanner in = new Scanner(System.in);
-		int selection = in.nextInt();
-		switch (selection) {
-		case 1:
-			addPurchaseMenu(household, budget);
-			break;
-		case 2:
-			SubPurchaseMenu subPurchaseMenu = new SubPurchaseMenu();
-			subPurchaseMenu.show(household, budget, new PurchaseMenu(), mainMenu);
-			break;
-		case 3:
-			System.out.println("Edit a purchase"); 
-			Purchase.editPurchases(household, budget);
-			break;
-		case 4: 
-			mainMenu.show(household, budget, mainMenu);
-			break;
-		}
-		show(household, budget, mainMenu);
+		boolean selectionError;
+		do {
+			try {
+				int selection = showOptions();
+				selectionError = false;
+				processSelection(household, budget, mainMenu, selection);
+			} catch (Exception e) {
+				selectionError = true;
+				System.out.println("Please enter a valid selection.");
+			}
+		} while (selectionError);
 	}
 
 	public static void addPurchaseMenu(Household household, Budget budget) {
@@ -43,22 +34,32 @@ public class PurchaseMenu extends Menu {
 
 	}
 
-//	public static void displayPurchasesMenu(Household household) {
-//		System.out.println(
-//				"Choose an option:\na - Show All Purchases\nb - Show Purchases By Family Member\nc - Show Purchases By Date");
-//		Scanner in = new Scanner(System.in);
-//		String selection = in.next();
-//		switch (selection) {
-//		case "a":
-//			Purchase.showAllPurchases(household);
-//			break;
-//		case "b":
-//			Purchase.showPurchasesByFamilyMember(household);
-//			break;
-//		case "c":
-//			Purchase.showPurchasesByDate(household.getPurchasesList());
-//			break;
-//		}
-//	}
+	public int showOptions() {
+		System.out.println(
+				"Purchase Menu Options:\n1 - Add a Purchase\n2 - Display Purchases\n3 - Edit a Purchase\n4 - Back to Main Menu");
+		Scanner in = new Scanner(System.in);
+		int selection = in.nextInt();
+		return selection;
+	}
+
+	public void processSelection(Household household, Budget budget, Menu mainMenu, int selection) {
+		switch (selection) {
+		case 1:
+			addPurchaseMenu(household, budget);
+			break;
+		case 2:
+			SubPurchaseMenu subPurchaseMenu = new SubPurchaseMenu();
+			subPurchaseMenu.show(household, budget, new PurchaseMenu(), mainMenu);
+			break;
+		case 3:
+			System.out.println("Edit a purchase");
+			Purchase.editPurchases(household, budget);
+			break;
+		case 4:
+			mainMenu.show(household, budget, mainMenu);
+			break;
+		}
+		show(household, budget, mainMenu);
+	}
 
 }

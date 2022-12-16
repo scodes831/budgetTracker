feb3fd7cdc21f3180beebe94c05ed46cb55b4598
scode234 <scodes3451@gmail.com>
2022-12-15 20:36:02 -0600

    Add abstract methods showOptions() and processSelection()

--- budgetTracker/src/budgetTracker/SubMenu.java
+++ budgetTracker/src/budgetTracker/SubMenu.java
@@ -3,5 +3,7 @@ package budgetTracker;
 public interface SubMenu {
 	
 	public abstract void show(Household household, Budget budget, Menu parentMenu, Menu mainMenu);
+	public abstract String showOptions();
+	public abstract void processSelection(Household household, Budget budget, Menu parentMenu, Menu mainMenu, String selection);
 
 }

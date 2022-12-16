46a1722853201089cc1c7f7c07e6ee096f0240bf
scode234 <scodes3451@gmail.com>
2022-12-15 20:27:12 -0600

    Refactor HouseholdMenu to handle invalid user input

--- budgetTracker/src/budgetTracker/HouseholdMenu.java
+++ budgetTracker/src/budgetTracker/HouseholdMenu.java
@@ -5,10 +5,29 @@ import java.util.Scanner;
 public class HouseholdMenu extends Menu {
 
 	public void show(Household household, Budget budget, Menu mainMenu) {
+		boolean selectionError;
+		do {
+			try {
+				int selection = showOptions();
+				selectionError = false;
+				processSelection(household, budget, mainMenu, selection);
+			} catch (Exception e) {
+				selectionError = true;
+				System.out.println("Please enter a valid selection.");
+			}
+		} while (selectionError);
+
+	}
+
+	public int showOptions() {
 		System.out.println(
 				"Household Menu Options:\n1 - Add a Family Member\n2 - Display Family Members\n3 - Edit Family Members\n4 - Back to Main Menu");
 		Scanner in = new Scanner(System.in);
 		int selection = in.nextInt();
+		return selection;
+	}
+
+	public void processSelection(Household household, Budget budget, Menu mainMenu, int selection) {
 		switch (selection) {
 		case 1:
 			household.addFamilyMembers();

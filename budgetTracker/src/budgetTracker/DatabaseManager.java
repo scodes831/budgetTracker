7f03578ae2464f58491ea13a5e70f91564752b57
scode234 <scodes3451@gmail.com>
2023-02-06 20:00:14 -0600

    Add createPurchasesTable method

--- budgetTracker/src/budgetTracker/DatabaseManager.java
+++ budgetTracker/src/budgetTracker/DatabaseManager.java
@@ -5,12 +5,12 @@ import java.sql.DriverManager;
 import java.sql.Statement;
 
 public class DatabaseManager {
-	
+
 	public Connection connectDatabase(String database, String user, String pw) {
 		Connection connection = null;
 		try {
 			Class.forName("org.postgresql.Driver");
-			connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/"+ database,user,pw);
+			connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/" + database, user, pw);
 			if (connection != null) {
 				System.out.println("connected");
 			}
@@ -19,11 +19,25 @@ public class DatabaseManager {
 		}
 		return connection;
 	}
-	
+
 	public void createUsersTable(Connection connection) {
 		Statement statement;
 		try {
 			String query = "CREATE TABLE IF NOT EXISTS users (userId SERIAL PRIMARY KEY, userName VARCHAR(30))";
+			statement = connection.createStatement();
+			statement.executeUpdate(query);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	public void createPurchasesTable(Connection connection) {
+		Statement statement;
+		try {
+			String query = "CREATE TABLE IF NOT EXISTS purchases (purchaseId SERIAL PRIMARY KEY, "
+					+ "purchaseDate DATE, category VARCHAR(20), "
+					+ "purchasedBy INTEGER, purchaseAmount NUMERIC(10,2), "
+					+ "FOREIGN KEY (purchasedBy) REFERENCES users(userId))";
 			statement = connection.createStatement();
 			statement.executeUpdate(query);
 		} catch (Exception e) {

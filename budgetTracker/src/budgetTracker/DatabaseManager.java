68984dfdc81287b46c8b63645045c303e2d9523b
scode234 <scodes3451@gmail.com>
2023-02-07 20:14:04 -0600

    Move purchases table methods to PurchasesTable class

--- budgetTracker/src/budgetTracker/DatabaseManager.java
+++ budgetTracker/src/budgetTracker/DatabaseManager.java
@@ -22,21 +22,7 @@ public class DatabaseManager {
 		}
 		return connection;
 	}
-
-	public void createPurchasesTable(Connection connection) {
-		Statement statement;
-		try {
-			String query = "CREATE TABLE IF NOT EXISTS purchases (purchaseId SERIAL PRIMARY KEY, "
-					+ "purchaseDate DATE, category VARCHAR(20), "
-					+ "purchasedBy INTEGER, purchaseAmount NUMERIC(10,2), "
-					+ "FOREIGN KEY (purchasedBy) REFERENCES users(userId))";
-			statement = connection.createStatement();
-			statement.executeUpdate(query);
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-	}
-
+	
 	public void createBudgetVsActualTable(Connection connection) {
 		Statement statement;
 		try {
@@ -45,23 +31,6 @@ public class DatabaseManager {
 					+ "spendAmount NUMERIC (10,2), remainingAmount NUMERIC(10,2))";
 			statement = connection.createStatement();
 			statement.executeUpdate(query);
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-	}
-
-	public void insertPurchasesRow(Connection connection, LocalDate purchaseDate, String category, String purchasedBy,
-			BigDecimal purchaseAmount) {
-		Statement statement;
-		try {
-			String query = String.format(
-					"insert into purchases (purchaseDate, category, purchasedBy, purchaseAmount) values ('%s','%s','%s','%s');",
-					java.sql.Date.valueOf(purchaseDate), category, getUserIdByUsername(connection, purchasedBy),
-					purchaseAmount);
-			statement = connection.createStatement();
-			statement.executeUpdate(query);
-			System.out.println("purchase row inserted");
-
 		} catch (Exception e) {
 			System.out.println(e);
 		}
--- budgetTracker/src/budgetTracker/PurchasesTable.java
+++ budgetTracker/src/budgetTracker/PurchasesTable.java
@@ -1,5 +1,40 @@
 package budgetTracker;
 
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.time.LocalDate;
+
 public class PurchasesTable {
 
+	public void createPurchasesTable(Connection connection) {
+		Statement statement;
+		try {
+			String query = "CREATE TABLE IF NOT EXISTS purchases (purchaseId SERIAL PRIMARY KEY, "
+					+ "purchaseDate DATE, category VARCHAR(20), "
+					+ "purchasedBy INTEGER, purchaseAmount NUMERIC(10,2), "
+					+ "FOREIGN KEY (purchasedBy) REFERENCES users(userId))";
+			statement = connection.createStatement();
+			statement.executeUpdate(query);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+	public void insertPurchasesRow(Connection connection, LocalDate purchaseDate, String category, String purchasedBy,
+			BigDecimal purchaseAmount) {
+		Statement statement;
+		try {
+			String query = String.format(
+					"insert into purchases (purchaseDate, category, purchasedBy, purchaseAmount) values ('%s','%s','%s','%s');",
+					java.sql.Date.valueOf(purchaseDate), category, getUserIdByUsername(connection, purchasedBy),
+					purchaseAmount);
+			statement = connection.createStatement();
+			statement.executeUpdate(query);
+			System.out.println("purchase row inserted");
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 }

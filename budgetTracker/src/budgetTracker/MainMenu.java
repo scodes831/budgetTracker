8181505c704f4abd09cd5c48e69e58f92b8dee77
scode234 <scodes3451@gmail.com>
2023-02-08 19:53:21 -0600

    Pass connection and tables parameters to processSelection and show methods

--- budgetTracker/src/budgetTracker/MainMenu.java
+++ budgetTracker/src/budgetTracker/MainMenu.java
@@ -1,16 +1,18 @@
 package budgetTracker;
 
+import java.sql.Connection;
 import java.util.Scanner;
 
 public class MainMenu extends Menu {
 
-	public void show(Household household, Budget budget, Menu mainMenu) {
+	public void show(Household household, Budget budget, Menu mainMenu, Connection connection, UsersTable usersTable,
+			BudgetActualTable budgetActualTable, PurchasesTable purchasesTable) {
 		boolean selectionError;
 		do {
 			try {
 				int selection = showOptions();
 				selectionError = false;
-				processSelection(household, budget, mainMenu, selection);
+				processSelection(household, budget, mainMenu, selection, connection, usersTable, budgetActualTable, purchasesTable);
 			} catch (Exception e) {
 				selectionError = true;
 				System.out.println("Please enter a valid selection.");
@@ -26,17 +28,19 @@ public class MainMenu extends Menu {
 		return selection;
 	}
 
-	public void processSelection(Household household, Budget budget, Menu mainMenu, int selection) {
+	public void processSelection(Household household, Budget budget, Menu mainMenu, int selection,
+			Connection connection, UsersTable usersTable, BudgetActualTable budgetActualTable,
+			PurchasesTable purchasesTable) {
 		switch (selection) {
 		case 1:
 			BudgetMenu budgetMenu = new BudgetMenu();
-			budgetMenu.show(household, budget, mainMenu);
+			budgetMenu.show(household, budget, mainMenu, connection, usersTable, budgetActualTable, purchasesTable);
 		case 2:
 			HouseholdMenu householdMenu = new HouseholdMenu();
-			householdMenu.show(household, budget, mainMenu);
+			householdMenu.show(household, budget, mainMenu, connection, usersTable, budgetActualTable, purchasesTable);
 		case 3:
 			PurchaseMenu purchaseMenu = new PurchaseMenu();
-			purchaseMenu.show(household, budget, mainMenu);
+			purchaseMenu.show(household, budget, mainMenu, connection, usersTable, budgetActualTable, purchasesTable);
 		}
 		show(household, budget, mainMenu);
 	}

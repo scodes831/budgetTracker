ac7514784885b6460d0f28019280231523fee8f6
scode234 <scodes3451@gmail.com>
2023-02-08 18:41:39 -0600

    Create usersTable and pass connection to addFamilyMember method

--- budgetTracker/src/budgetTracker/Main.java
+++ budgetTracker/src/budgetTracker/Main.java
@@ -1,13 +1,22 @@
 package budgetTracker;
 
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.time.LocalDate;
+
 public class Main {
 	
 	public static void main(String[] args) {
+		DatabaseManager dbManager = new DatabaseManager();
+		Connection connection = dbManager.connectDatabase("budgettracker", null, null);
+		UsersTable usersTable = new UsersTable();
+		usersTable.createUsersTable(connection);
+		
 		Menu.welcomeUser();
 		Household household = new Household();
 		Budget budget = Budget.initializeBudget(household, Household.generateBudgetName());
 		budget.setUpBudget(household, budget);
-		household.addFamilyMembers();
+		household.addFamilyMembers(connection, usersTable);
 		MainMenu mainMenu = new MainMenu();
 		mainMenu.show(household, budget, mainMenu);
 	}

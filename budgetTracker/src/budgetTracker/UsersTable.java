e629331a6a6577f7b09a9d1d039255bf927b7b11
scode234 <scodes3451@gmail.com>
2023-02-09 19:50:18 -0600

    Add updateUsername method

--- budgetTracker/src/budgetTracker/UsersTable.java
+++ budgetTracker/src/budgetTracker/UsersTable.java
@@ -5,7 +5,7 @@ import java.sql.Connection;
 import java.sql.Statement;
 
 public class UsersTable {
-	
+
 	public void createUsersTable(Connection connection) {
 		Statement statement;
 		try {
@@ -17,7 +17,7 @@ public class UsersTable {
 			System.out.println(e);
 		}
 	}
-	
+
 	public void insertUsersRow(Connection connection, String username, BigDecimal salary) {
 		Statement statement;
 		try {
@@ -25,6 +25,20 @@ public class UsersTable {
 			statement = connection.createStatement();
 			statement.executeUpdate(query);
 			System.out.println("users row inserted");
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	public void updateUsername(Connection connection, String newUsername, String oldUsername) {
+		Statement statement;
+		try {
+			String query = String.format("update users set username = '%s' WHERE userid = '%s');", newUsername,
+					DatabaseManager.getUserIdByUsername(connection, oldUsername));
+			statement = connection.createStatement();
+			statement.executeUpdate(query);
+			System.out.println("users row updated");
+
 		} catch (Exception e) {
 			System.out.println(e);
 		}

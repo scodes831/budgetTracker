c73491110ffbf31a0afd61e16c31c06fad7eeade
scode234 <scodes3451@gmail.com>
2023-02-14 18:19:22 -0600

    Add connection and table parameters to corresponding methods

--- budgetTracker/src/budgetTracker/SubMenu.java
+++ budgetTracker/src/budgetTracker/SubMenu.java
@@ -1,9 +1,13 @@
 package budgetTracker;
 
+import java.sql.Connection;
+
 public interface SubMenu {
 	
-	public abstract void show(Household household, Budget budget, Menu parentMenu, Menu mainMenu);
+	public abstract void show(Household household, Budget budget, Menu parentMenu, Menu mainMenu, Connection connection, UsersTable usersTable,
+			BudgetActualTable budgetActualTable, PurchasesTable purchasesTable);
 	public abstract String showOptions();
-	public abstract void processSelection(Household household, Budget budget, Menu parentMenu, Menu mainMenu, String selection);
+	public abstract void processSelection(Household household, Budget budget, Menu parentMenu, Menu mainMenu, String selection, Connection connection, UsersTable usersTable,
+			BudgetActualTable budgetActualTable, PurchasesTable purchasesTable);
 
 }

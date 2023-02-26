8d32ebf10a5d6afe10e3e7a9dbbc0e1822a8ae42
scode234 <scodes3451@gmail.com>
2023-02-26 07:38:52 -0600

    Call methods

--- budgetTracker/src/budgetTracker/Main.java
+++ budgetTracker/src/budgetTracker/Main.java
@@ -1,21 +1,17 @@
 package budgetTracker;
 
-import java.math.BigDecimal;
 import java.sql.Connection;
-import java.time.LocalDate;
 
 public class Main {
 	
 	public static void main(String[] args) {
-		DatabaseManager dbManager = new DatabaseManager();
-		Connection connection = dbManager.connectDatabase("budgettracker", null, null);
+		Connection connection = DatabaseManager.connectDatabase("budgettracker", null, null);
 		UsersTable usersTable = new UsersTable();
 		usersTable.createUsersTable(connection);
 		BudgetActualTable budgetActualTable = new BudgetActualTable();
 		budgetActualTable.createBudgetVsActualTable(connection);
 		PurchasesTable purchasesTable = new PurchasesTable();
 		purchasesTable.createPurchasesTable(connection);
-		
 		Menu.welcomeUser();
 		Household household = new Household();
 		Budget budget = Budget.initializeBudget(household, Household.generateBudgetName());
@@ -25,7 +21,3 @@ public class Main {
 		mainMenu.show(household, budget, mainMenu, connection, usersTable, budgetActualTable, purchasesTable);
 	}
 }
-
-//need to fix:
-//1. budgetname and category cannot be unique in budget table but can only have one combo of same budgetname and category
-//2.

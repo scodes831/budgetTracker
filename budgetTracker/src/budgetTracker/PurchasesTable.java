71d292c3f36ea432e4c2ce59cd3be95d4ad3717e
scode234 <scodes3451@gmail.com>
2023-03-02 20:17:15 -0600

    Add readAllPurchases method

--- budgetTracker/src/budgetTracker/PurchasesTable.java
+++ budgetTracker/src/budgetTracker/PurchasesTable.java
@@ -2,6 +2,7 @@ package budgetTracker;
 
 import java.math.BigDecimal;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.Statement;
 import java.time.LocalDate;
 
@@ -28,7 +29,8 @@ public class PurchasesTable {
 			String query = String.format(
 					"insert into purchases (purchaseDate, category, purchasedBy, purchaseAmount) values ('%s','%s','%s','%s');",
 					java.sql.Date.valueOf(purchaseDate), category,
-					DatabaseManager.getUserIdByUsername(connection, FamilyMember.capitalizeName(purchasedBy)), purchaseAmount);
+					DatabaseManager.getUserIdByUsername(connection, FamilyMember.capitalizeName(purchasedBy)),
+					purchaseAmount);
 			statement = connection.createStatement();
 			statement.executeUpdate(query);
 		} catch (Exception e) {
@@ -42,12 +44,50 @@ public class PurchasesTable {
 		try {
 			String query = String.format(
 					"update purchases set purchasedate = '%s', category = '%s', purchasedby = '%s', purchaseamount = '%s' where purchaseid = '%s';",
-					java.sql.Date.valueOf(newPurchaseDate), newCategory, DatabaseManager.getUserIdByUsername(connection, newPurchasedBy), newPurchaseAmount, purchaseId);
+					java.sql.Date.valueOf(newPurchaseDate), newCategory,
+					DatabaseManager.getUserIdByUsername(connection, newPurchasedBy), newPurchaseAmount, purchaseId);
 			statement = connection.createStatement();
 			statement.executeUpdate(query);
 		} catch (Exception e) {
 			System.out.println(e);
 		}
 
+	}
+
+	public void readAllPurchases(Connection connection, Household household) {
+		Statement statement;
+		ResultSet result;
+		try {
+			String query = "select * from purchases;";
+			statement = connection.createStatement();
+			result = statement.executeQuery(query);
+			while (result.next()) {
+				boolean alreadyExists = false;
+				String[] purchaseDate = result.getString("purchasedate").split("-");
+				int purchYear = Integer.valueOf(purchaseDate[0]);
+				int purchMonth = Integer.valueOf(purchaseDate[1]);
+				int purchDay = Integer.valueOf(purchaseDate[2]);
+				LocalDate purchDate = LocalDate.of(purchYear, purchMonth, purchDay);
+				String purchCategory = result.getString("category");
+				double purchAmount = Double.valueOf(result.getString("purchaseamount"));
+				String purchBy = DatabaseManager.getUsernameByUserId(connection,
+						Integer.valueOf(result.getString("purchasedby")));
+				for (int i = 0; i < household.getPurchasesList().size(); i++) {
+					if (household.getPurchasesList().get(i).getCategory().equals(purchCategory)
+							&& household.getPurchasesList().get(i).getAmount() == purchAmount
+							&& household.getPurchasesList().get(i).getPurchasedBy().equals(purchBy)
+							&& household.getPurchasesList().get(i).getDatePurchased() == purchDate) {
+						alreadyExists = true;
+					}
+				}
+
+				if (!alreadyExists) {
+					Purchase currPurchase = new Purchase(purchCategory, purchAmount, purchBy, purchDate);
+					household.getPurchasesList().add(currPurchase);
+				}
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
 }

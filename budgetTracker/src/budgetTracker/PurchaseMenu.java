932b878a8d5fdbdbc237e57f00e7a391c842c3d0
scode234 <scodes3451@gmail.com>
2023-03-06 18:36:19 -0600

    Call readAllPurchases and readAllUsers methods

--- budgetTracker/src/budgetTracker/PurchaseMenu.java
+++ budgetTracker/src/budgetTracker/PurchaseMenu.java
@@ -46,13 +46,17 @@ public class PurchaseMenu extends Menu {
 			BudgetActualTable budgetActualTable, PurchasesTable purchasesTable) {
 		switch (selection) {
 		case 1:
+			usersTable.readAllUsers(connection, household);
 			addPurchaseMenu(household, connection, usersTable, purchasesTable);
 			break;
 		case 2:
+			purchasesTable.readAllPurchases(connection, household);
 			SubPurchaseMenu subPurchaseMenu = new SubPurchaseMenu();
 			subPurchaseMenu.show(household, new PurchaseMenu(), mainMenu, connection, usersTable, budgetActualTable, purchasesTable);
 			break;
 		case 3:
+			purchasesTable.readAllPurchases(connection, household);
+			usersTable.readAllUsers(connection, household);
 			System.out.println("Edit a purchase");
 			Purchase.editPurchases(household, connection, usersTable, purchasesTable);
 			break;

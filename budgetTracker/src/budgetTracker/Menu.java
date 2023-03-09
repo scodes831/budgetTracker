d6de74e704d6490e85375e8544312749ad79aa4e
scode234 <scodes3451@gmail.com>
2023-03-08 19:52:07 -0600

    Remove unused method

--- budgetTracker/src/budgetTracker/Menu.java
+++ budgetTracker/src/budgetTracker/Menu.java
@@ -7,8 +7,8 @@ public abstract class Menu {
 
 	private static ArrayList<String> activeMenuList = new ArrayList<String>();
 
-	public abstract void show(Household household, Menu mainMenu, Connection connection,
-			UsersTable usersTable, BudgetActualTable budgetActualTable, PurchasesTable purchasesTable);
+	public abstract void show(Household household, Menu mainMenu, Connection connection, UsersTable usersTable,
+			BudgetActualTable budgetActualTable, PurchasesTable purchasesTable);
 
 	public abstract int showOptions();
 
@@ -17,15 +17,6 @@ public abstract class Menu {
 
 	public static void welcomeUser() {
 		System.out.println("Welcome to Budget Tracker! Follow the prompts to get started.\n");
-	}
-
-	public static void getNextMenu() {
-		int length = Menu.getActiveMenuList().size();
-		String currentMenu = Menu.getActiveMenuList().get(length - 1);
-		switch (currentMenu) {
-		case "main":
-
-		}
 	}
 
 	public static ArrayList<String> getActiveMenuList() {

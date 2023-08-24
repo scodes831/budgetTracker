e9b9e41c8e0f4423ea795da02aa97df6e3dba578
scode234 <scodes3451@gmail.com>
2023-08-23 19:07:06 -0500

    Add overloaded methods createSpreadsheet and readData to handle purchase exports

--- budgetTracker/src/budgetTracker/SpreadsheetManager.java
+++ budgetTracker/src/budgetTracker/SpreadsheetManager.java
@@ -1,13 +1,17 @@
 package budgetTracker;
 
 import java.io.FileOutputStream;
-import java.math.BigDecimal;
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -21,7 +25,6 @@ public class SpreadsheetManager {
 		LinkedHashMap<String, String> columnHeaders = new LinkedHashMap<String, String>();
 
 		switch (reportType) {
-
 		case "monthly budget":
 			System.out.println("Exporting monthly budget");
 			columnHeaders.put("category", "String");
@@ -39,6 +42,30 @@ public class SpreadsheetManager {
 		columnHeaders.clear();
 	}
 
+	public void createSpreadsheet(ResultSet data, Connection connection, String reportType, Household household)
+			throws Exception {
+
+		XSSFWorkbook workbook = new XSSFWorkbook();
+		XSSFSheet sheet = workbook.createSheet();
+		LinkedHashMap<String, String> columnHeaders = new LinkedHashMap<String, String>();
+
+		switch (reportType) {
+		case "all purchases":
+			System.out.println("Exporting all purchases");
+			columnHeaders.put("purchasedate", "Date");
+			columnHeaders.put("category", "String");
+			columnHeaders.put("purchasedby", "String");
+			columnHeaders.put("purchaseamount", "Big Decimal");
+		}
+
+		readData(data, connection, workbook, sheet, columnHeaders, household);
+		String filePath = ".//exports/" + generateFileName() + "_all_purchases.xlsx";
+		FileOutputStream out = new FileOutputStream(filePath);
+		workbook.write(out);
+		out.close();
+		columnHeaders.clear();
+	}
+
 	private void readData(ResultSet data, XSSFWorkbook workbook, XSSFSheet sheet,
 			LinkedHashMap<String, String> columnHeaders, Budget budget) throws Exception {
 		XSSFRow headerRow = sheet.createRow(0);
@@ -46,14 +73,13 @@ public class SpreadsheetManager {
 		int columns = 0;
 		for (String key : keys) {
 			headerRow.createCell(columns++).setCellValue(key);
-			
 		}
-		
+
 		int rowNum = 1;
 		while (data.next()) {
 			XSSFRow row = sheet.createRow(rowNum++);
 			columns = 0;
-			for (Map.Entry<String,String> entry : columnHeaders.entrySet()) {
+			for (Map.Entry<String, String> entry : columnHeaders.entrySet()) {
 				switch (entry.getValue()) {
 				case "String":
 					String str = data.getString(entry.getKey());
@@ -66,12 +92,55 @@ public class SpreadsheetManager {
 				}
 			}
 		}
-		 XSSFRow totals = sheet.createRow(rowNum);
-		 totals.createCell(0).setCellValue("Total:");
-		 totals.createCell(1).setCellValue(budget.getTotalBudgeted().doubleValue());
-		 totals.createCell(2).setCellValue(budget.getTotalSpent().doubleValue());
-		 totals.createCell(3).setCellValue(budget.getTotalRemaining().doubleValue());
-		  
+		XSSFRow totals = sheet.createRow(rowNum);
+		totals.createCell(0).setCellValue("Total:");
+		totals.createCell(1).setCellValue(budget.getTotalBudgeted().doubleValue());
+		totals.createCell(2).setCellValue(budget.getTotalSpent().doubleValue());
+		totals.createCell(3).setCellValue(budget.getTotalRemaining().doubleValue());
+
+	}
+
+	private void readData(ResultSet data, Connection connection, XSSFWorkbook workbook, XSSFSheet sheet,
+			LinkedHashMap<String, String> columnHeaders, Household household) throws Exception {
+		XSSFRow headerRow = sheet.createRow(0);
+		Set<String> keys = columnHeaders.keySet();
+		int columns = 0;
+		for (String key : keys) {
+			headerRow.createCell(columns++).setCellValue(key);
+		}
+
+		int rowNum = 1;
+		while (data.next()) {
+			XSSFRow row = sheet.createRow(rowNum++);
+			columns = 0;
+			for (Map.Entry<String, String> entry : columnHeaders.entrySet()) {
+				if (entry.getKey().equals("purchasedby")) {
+					String purchasedByName = DatabaseManager.getUsernameByUserId(connection, Integer.valueOf(data.getString(entry.getKey())));
+					row.createCell(columns++).setCellValue(purchasedByName);
+				} else {
+					switch (entry.getValue()) {
+					case "String":
+						String str = data.getString(entry.getKey());
+						row.createCell(columns++).setCellValue(str);
+						break;
+					case "Big Decimal":
+						double bd = data.getDouble(entry.getKey());
+						row.createCell(columns++).setCellValue(bd);
+						break;
+					case "Date":
+						String[] strDate = data.getString(entry.getKey()).split("-");
+						Calendar date = new GregorianCalendar();
+						date.set(Integer.valueOf(strDate[0]), Integer.valueOf(strDate[1])-1, Integer.valueOf(strDate[2])); 
+						XSSFCellStyle dateStyle = workbook.createCellStyle();
+						dateStyle.setDataFormat((short) 14);
+						XSSFCell cell = row.createCell(columns++);
+						cell.setCellValue(date);
+						cell.setCellStyle(dateStyle);
+						break;
+					}
+				}
+			}
+		}
 	}
 
 	private String generateFileName() {

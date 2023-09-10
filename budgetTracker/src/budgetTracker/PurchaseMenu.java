b0d88c6ffb4c61d08115d607826191b5193489b0
scode234 <scodes3451@gmail.com>
2023-09-09 20:06:34 -0500

    Pass budgetTable as argument to addPurchase and read purchase table when adding a new purchase

--- budgetTracker/src/budgetTracker/PurchaseMenu.java
+++ budgetTracker/src/budgetTracker/PurchaseMenu.java
@@ -14,7 +14,8 @@ public class PurchaseMenu extends Menu {
 			try {
 				int selection = showOptions();
 				selectionError = false;
-				processSelection(household, mainMenu, selection, connection, usersTable, budgetActualTable, purchasesTable);
+				processSelection(household, mainMenu, selection, connection, usersTable, budgetActualTable,
+						purchasesTable);
 			} catch (Exception e) {
 				selectionError = true;
 				System.out.println("Please enter a valid selection.");
@@ -22,14 +23,16 @@ public class PurchaseMenu extends Menu {
 		} while (selectionError);
 	}
 
-	public static void addPurchaseMenu(Household household, Connection connection, UsersTable usersTable, PurchasesTable purchasesTable) {
+	public static void addPurchaseMenu(Household household, Connection connection, UsersTable usersTable,
+			PurchasesTable purchasesTable, BudgetActualTable budgetTable) {
 		boolean purchaseAdded = false;
 		do {
-			LocalDate datePurchased = PromptUserInput.promptUserDateInput(household, connection, usersTable, purchasesTable);
+			LocalDate datePurchased = PromptUserInput.promptUserDateInput(household, connection, usersTable,
+					purchasesTable, budgetTable);
 			String purchasedBy = PromptUserInput.promptUserNameInput(household, connection, usersTable, purchasesTable);
 			BigDecimal amount = PromptUserInput.promptUserAmountInput(household);
 			String category = PromptUserInput.promptUserCategoryInput(household);
-			household.addPurchase(category, amount, purchasedBy, datePurchased, connection, purchasesTable);
+			household.addPurchase(category, amount, purchasedBy, datePurchased, connection, purchasesTable, budgetTable);
 			purchaseAdded = true;
 		} while (!purchaseAdded);
 
@@ -43,23 +46,26 @@ public class PurchaseMenu extends Menu {
 		return selection;
 	}
 
-	public void processSelection(Household household, Menu mainMenu, int selection, Connection connection, UsersTable usersTable,
-			BudgetActualTable budgetActualTable, PurchasesTable purchasesTable) {
+	public void processSelection(Household household, Menu mainMenu, int selection, Connection connection,
+			UsersTable usersTable, BudgetActualTable budgetActualTable, PurchasesTable purchasesTable) {
 		switch (selection) {
 		case 1:
 			usersTable.readAllUsers(connection, household);
-			addPurchaseMenu(household, connection, usersTable, purchasesTable);
+			budgetActualTable.readAllBudgetNames(connection, household);
+			purchasesTable.readAllPurchases(connection, household);
+			addPurchaseMenu(household, connection, usersTable, purchasesTable, budgetActualTable);
 			break;
 		case 2:
 			purchasesTable.readAllPurchases(connection, household);
 			SubPurchaseMenu subPurchaseMenu = new SubPurchaseMenu();
-			subPurchaseMenu.show(household, new PurchaseMenu(), mainMenu, connection, usersTable, budgetActualTable, purchasesTable);
+			subPurchaseMenu.show(household, new PurchaseMenu(), mainMenu, connection, usersTable, budgetActualTable,
+					purchasesTable);
 			break;
 		case 3:
 			purchasesTable.readAllPurchases(connection, household);
 			usersTable.readAllUsers(connection, household);
 			System.out.println("Edit a purchase");
-			Purchase.editPurchases(household, connection, usersTable, purchasesTable);
+			Purchase.editPurchases(household, connection, usersTable, purchasesTable, budgetActualTable);
 			break;
 		case 4:
 			usersTable.readAllUsers(connection, household);
